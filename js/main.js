7988d9641a314ca6b728a4352e81fc3f8ab00a42
Jacob <jakeboone@aol.com>
2014-06-23 11:14:20 -0600

    [ENTER MESSAGE]

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+console.log('Hello Boulder!');
+
+
+
+
+// var x = 5000;
+
+// if(x < 1000) {
+// 	console.log("Tiny");
+// }
+
+// else { 
+// 	console.log("Not so tiny");
+// }
+
+
+// var first = "Jacob";
+// var last = "Davidson";
+// alert("Hello " + first + " " + last + "!");
